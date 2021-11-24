97f487521369561c43ca1524181a5b78f56ecdae
oajh04 <cloudtubekr@naver.com>
2021-11-24 20:45:39 +0900

    ⚙️ :: 세팅

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { MainRouter } from './Router';
+
+function App() {
+  return (
+    <>
+      <MainRouter />
+    </>
+  );
+}
+
+export default App;
