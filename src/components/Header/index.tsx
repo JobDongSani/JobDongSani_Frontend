50d06ec55af49caf05c5dc5692d8ee12db4cdeb7
oajh04 <cloudtubekr@naver.com>
2021-11-24 20:45:49 +0900

    ⚡️ :: Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Header = () => {
+  return (
+    <>
+        <HeaderWrapper>
+            <HeaderContent>
+                <LogoBox>
+                    <div>fhrh</div>
+                    <Title>잡동사니</Title>
+                </LogoBox>
+                
+                <ListBox>
+                    <List>잡동사니</List>
+                    <List>잡동사니</List>
+                    <List>잡동사니</List>
+                    <List>로그인</List>
+                </ListBox>
+            </HeaderContent>
+        </HeaderWrapper>
+    </>
+  );
+}
+
+export default Header;
+
+const HeaderWrapper = styled.div`
+    width: 100%;
+    height: 45px;
+    position: sticky;
+    border-bottom: 1px solid black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const HeaderContent = styled.div`
+    width: calc(100% - 300px);
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+`
+
+const LogoBox = styled.div`
+    width: 120px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const Title = styled.div`
+    font-size: 20px;
+    font-weight: bold;
+    color: #7190FF;
+`
+
+const ListBox = styled.ul`
+    margin: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+`
+
+const List = styled.li`
+    width: 100px;
+    height: 45px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    list-style: none;
+    cursor: pointer;
+    z-index: 99;
+    position: relative;
+    ::before, ::after{
+        content: '';
+        border-bottom: solid 2px #7190FF;
+        position: absolute;
+        bottom: 0;
+        width: 0;
+        -webkit-transition: all 0.2s ease;
+            transition: all 0.22s ease;
+    }
+    ::before {
+        left: 50px;
+    }
+    ::after{
+        right: 50px;
+    }
+    &:hover::before, &:hover::after{
+        width: 50px;
+    }
+`
