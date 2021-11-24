4639c4e78da54a67dab5bddf9c827c61058dcab9
oajh04 <cloudtubekr@naver.com>
2021-11-24 20:46:50 +0900

    ⚡️ :: 회원가입 api 저장

---
+++ src/libs/api/auth/index.ts
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+import request from '../../axios'
+
+export default {
+    postRegister(id: string, name: string, password: string, cellphone: number | string, profileUrl: string){
+        return request({
+            url: '/signup',
+            method: 'post',
+            data: {
+                username: id,
+                name: name,
+                password: password,
+                phoneNumber: cellphone,
+                userImage: profileUrl
+            }
+        })
+    }
+}
