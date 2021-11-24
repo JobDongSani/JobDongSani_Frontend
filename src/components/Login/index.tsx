869e87b232e145c0a7c32eae0a594cb1e7608a0e
oajh04 <cloudtubekr@naver.com>
2021-11-24 20:46:14 +0900

    ⚡️ :: Login 페이지 생성

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Login = () => {
+  return (
+    <>
+        <LoginWrapper>
+            
+        </LoginWrapper>
+    </>
+  );
+}
+
+export default Login;
+
+const LoginWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+`
