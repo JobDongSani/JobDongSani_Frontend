68b3414bafcf976f3a66625e8a1fd49119919961
oajh04 <cloudtubekr@naver.com>
2021-11-25 16:32:25 +0900

    ⚡️ :: Home

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChallengeSlide from './ChallengeSlide'
+
+const Home = () => {
+  return (
+    <>
+        <ChallengeSlide />
+    </>
+  );
+}
+
+export default Home;
