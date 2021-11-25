4478839c0994eb76a5a958642e708460f13d7d40
oajh04 <cloudtubekr@naver.com>
2021-11-25 16:31:34 +0900

    ⚡️:: 챌린지

--- src/components/Challenge/index.tsx
+++ src/components/Challenge/index.tsx
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import ChallengeCard from './ChallengeCard';
+import challenge from '../../libs/api/challenge';
+import { AiOutlineSearch } from 'react-icons/ai'
+import { useNavigate } from 'react-router';
 
 const Challenge = () => {
+    const navigate = useNavigate()
+    const [ challengeData, setChallengeData ] = useState<any[]>([])
+    const [ search, setSearch ] = useState('');
+
+    useEffect(()=> {
+        challenge.getChallengeList(0)
+        .then((res) => {
+            console.log(res.data.data.responses)
+            setChallengeData(res.data.data.responses)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    },[])
+
+    const onSearch = (e: any) => {
+        e.preventDefault()
+        challenge.getChallengeSearch(search, 0)
+        .then((res) => {
+            console.log(res)
+            setChallengeData(res.data.data.responses)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
   return (
     <>
         <ChallengeWrapper>
+            <ChallengeInputBox>
+                <ChallengeAdd onClick={()=>navigate('/cardAdd/challenge-post')}>챌린지 생성</ChallengeAdd>
+                <ChallengeInput placeholder="검색" onChange={(e: any) => setSearch(e.target.value)}/>
+                <ChallengeButton onClick={onSearch}><AiOutlineSearch/></ChallengeButton>
+                <select>
+                    <option>최신순</option>
+                    <option>좋아요순</option>
+                </select>
+            </ChallengeInputBox>
+
             <ChallengeList>
             {
-                Array(11).fill(-1).map((_, index: number)=> {
+                challengeData !== [] &&
+                challengeData.map((i: any, index: number)=> {
                     return (
-                        <ChallengeCard/>
+                        <ChallengeCard {...i} key={i.id}/>
                     )
                 })
             }
@@ -21,6 +62,52 @@ const Challenge = () => {
 }
 
 export default Challenge;
+
+const ChallengeInputBox = styled.form`
+    width: 1300px;
+    margin: 30px auto;
+    margin-top: 50px;
+    display: flex;
+    align-items: flex-end;
+    justify-content: right;
+    @media screen and (max-width: 1600px) { 
+        width: 1000px;
+    }
+`
+
+const ChallengeAdd = styled.div`
+    width: 100px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid black;
+    border-radius: 10px;
+    margin-right: 10px;
+    cursor: pointer;
+`
+
+const ChallengeInput = styled.input`
+    width: 200px;
+    padding: 5px;
+    font-size: 16px;
+    outline: none;
+    border: none;
+    border-bottom: 1px solid black;
+`
+
+const ChallengeButton = styled.button`
+    background-color: none;
+    background: none;
+    border: none;
+    border-bottom: 1px solid black;
+    margin-right: 10px;
+
+    svg{
+        width: 30px;
+        height: 30px;
+    }
+`
 
 const ChallengeWrapper = styled.div`
     margin: auto;
